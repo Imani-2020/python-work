774ecd9886447a52978f69cf326a8aaf203b79c9
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-20 15:47:05 -0500

    two todos

---
+++ variables.py
@@ -0,0 +1,21 @@
+name = "Imani"
+age= 17
+print("My name is",name, "and I am " ,age, )
+randomNum = 1.0
+futureAge = randomNum + age
+
+print("Next year I will be", futureAge)
+
+randomInt=5
+randomfloat=7.2
+randomBool= True
+randomNone= None
+randomString ="sup"
+
+print(type(randomInt))
+print(type(randomfloat))
+print(type(randomBool))
+print(type(randomNone))
+print(type(randomString))
+
+print("My name is"+ name + " and I am "+ str(age))
